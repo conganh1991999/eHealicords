4ab9d9a84dbd3f5ce943d8d2bac74d89596c6e58
conganh1991999 <conganh1991999@gmail.com>
2021-09-20 00:30:59 +0700

    init project

---
+++ src/main/java/com/anhnc2/ehealicords/EHealicordsApplication.java
@@ -0,0 +1,13 @@
+package com.anhnc2.ehealicords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealicordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHealicordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anhnc2/ehealicords/EHealicordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhnc2.ehealicords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealicordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
