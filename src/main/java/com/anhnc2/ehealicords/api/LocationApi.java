9ea0f1cc7c69b1da3cca8531b29035229df84d46
conganh1991999 <conganh1991999@gmail.com>
2021-10-16 12:34:17 +0700

    add location api (s)

---
+++ src/main/java/com/anhnc2/ehealicords/api/LocationApi.java
@@ -0,0 +1,51 @@
+package com.anhnc2.ehealicords.api;
+
+import com.anhnc2.ehealicords.constant.StatusCode;
+import com.anhnc2.ehealicords.data.entity.DistrictEntity;
+import com.anhnc2.ehealicords.data.entity.ProvinceEntity;
+import com.anhnc2.ehealicords.data.entity.WardEntity;
+import com.anhnc2.ehealicords.data.response.HttpResponse;
+import com.anhnc2.ehealicords.data.response.HttpResponseImpl;
+import com.anhnc2.ehealicords.service.location.LocationService;
+import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/protected/locations")
+@AllArgsConstructor
+public class LocationApi {
+    private final LocationService locationService;
+
+    @GetMapping("/provinces")
+    @Cacheable(cacheNames = "provinces")
+    public HttpResponse<List<ProvinceEntity>> getProvinceList() {
+        return HttpResponseImpl.<List<ProvinceEntity>>builder()
+                .code(StatusCode.SUCCESS)
+                .data(locationService.getProvinces())
+                .build();
+    }
+
+    @GetMapping("/provinces/{provinceId}/districts")
+    @Cacheable(cacheNames = "districts")
+    public HttpResponse<List<DistrictEntity>> getDistrictList(
+            @PathVariable(name = "provinceId") int provinceId) {
+        return HttpResponseImpl.<List<DistrictEntity>>builder()
+                .data(locationService.getDistricts(provinceId))
+                .build();
+    }
+
+    @GetMapping("/districts/{districtId}/wards")
+    @Cacheable("wards")
+    public HttpResponse<List<WardEntity>> getWardList(
+            @PathVariable(name = "districtId") int districtId) {
+        return HttpResponseImpl.<List<WardEntity>>builder()
+                .data(locationService.getWards(districtId))
+                .build();
+    }
+}
